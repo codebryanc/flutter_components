5d913f4cea05ff83f507258dea7a275c1d9bd76c
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-01-06 08:40:17 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_temp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', debugShowCheckedModeBanner: false, home: HomePageTemp());
+  }
+}
