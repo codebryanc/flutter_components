2d63c8b328182aa4a208a4818f2b8e00610d9e0a
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-08-03 10:23:44 -0500

    Path in main dart page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         '/'       : (BuildContext context) => HomePage(),
         'alert'   : (BuildContext context) => AlertPage(),
         'avatar'  : (BuildContext context) => AvatarPage(),
-      }
+      },
     );
   }
 }
